ba69c1cdb4dbfdc70856fc1bab14c48255afa763
Paweł Sobieraj <pwsb@dgs-diagnostics.com>
2022-08-25 15:23:06 +0200

    Adding benchmarks

---
+++ SDAOponeo.UnitTests.Benchmark/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Running;
+using SDAOponeo.UnitTests.Benchmark.Tests;
+
+var summary = BenchmarkRunner.Run<StringDigitComparator>();
+Console.ReadKey();
---
+++ SDAOponeo.UnitTests.Benchmark/Tests/StringDigitComparator.cs
@@ -0,0 +1,33 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using System.Text.RegularExpressions;
+
+namespace SDAOponeo.UnitTests.Benchmark.Tests
+{
+    [SimpleJob(RunStrategy.ColdStart, targetCount: 1000)]
+    [MinColumn, MaxColumn, MeanColumn, MedianColumn]
+    public class StringDigitComparator
+    {
+        [Benchmark]
+        [Arguments("3215532421213321321")]
+        public bool IsStringDigitUsingLinq(string str)
+        {
+            return str.All(char.IsDigit);
+        }
+
+        [Benchmark]
+        [Arguments("3215532421213321321")]
+        public bool IsStringDigitUsingRegex(string str)
+        {
+            return Regex.IsMatch(str, @"^\d+$");
+        }
+
+        [Benchmark]
+        [Arguments("3215532421213321321")]
+        public bool IStringDigitUsingDoubleParse(string str)
+        {
+            bool isNum = Double.TryParse(Convert.ToString(str), System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out double retNum);
+            return isNum;
+        }
+    }
+}
